bbf34e7ce88342ba5ccadec76b0d4353b5444351
Graham Bewley <grahambewley@gmail.com>
2021-01-13 23:28:10 -0500

    Add little icon to title

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import classes from './Header.module.css';
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRecordVinyl } from '@fortawesome/free-solid-svg-icons';
 
 const Header = ({ userData }) => {
   return (
     <header className={classes.container}>
       <Link href="/">
         <a className={classes.tempLogoLink}>
+          <FontAwesomeIcon
+            className={classes.tempLogoIcon}
+            icon={faRecordVinyl}
+          />
           <h3 className={classes.tempLogo}>FindOnVinyl</h3>
         </a>
       </Link>
