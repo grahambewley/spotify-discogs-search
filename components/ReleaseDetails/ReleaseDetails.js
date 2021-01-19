fcf3ceff2d4a546b4495d5d30214774c6672d013
Graham Bewley <grahambewley@gmail.com>
2021-01-19 08:24:03 -0500

    Add track details styling

--- components/ReleaseDetails/ReleaseDetails.js
+++ components/ReleaseDetails/ReleaseDetails.js
@@ -2,40 +2,87 @@ import React from 'react';
 import classes from './ReleaseDetails.module.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faGlobe, faCalendar } from '@fortawesome/free-solid-svg-icons';
+import {
+  faUser,
+  faGlobe,
+  faCalendar,
+  faMusic
+} from '@fortawesome/free-solid-svg-icons';
 
 const ReleaseDetails = ({ release }) => {
   return (
-    <div className={classes.releaseInfo}>
-      <h3 className={classes.releaseName}>{release.spotifyAlbumName}</h3>
-      {release.spotifyArtist && (
-        <h4 className={classes.releaseDetail}>
-          <FontAwesomeIcon
-            className={classes.releaseDetailIcon}
-            icon={faUser}
-          />
-          {release.spotifyArtist}
-        </h4>
+    <>
+      {release.spotifyTrackName ? (
+        <div className={classes.releaseInfo}>
+          <h3 className={classes.releaseName}>{release.spotifyAlbumName}</h3>
+          <h4 className={classes.releaseDetail}>
+            <FontAwesomeIcon
+              icon={faMusic}
+              className={classes.releaseDetailIcon}
+            />
+            {release.spotifyTrackName} &mdash; {release.spotifyArtist}
+          </h4>
+          {/* {release.spotifyArtist && (
+            <h4 className={classes.releaseDetail}>
+              <FontAwesomeIcon
+                className={classes.releaseDetailIcon}
+                icon={faUser}
+              />
+              {release.spotifyArtist}
+            </h4>
+          )} */}
+          {release.releaseCountry && (
+            <h4 className={classes.releaseDetail}>
+              <FontAwesomeIcon
+                className={classes.releaseDetailIcon}
+                icon={faGlobe}
+              />
+              {release.releaseCountry}
+            </h4>
+          )}
+          {release.releaseYear && (
+            <h4 className={classes.releaseDetail}>
+              <FontAwesomeIcon
+                className={classes.releaseDetailIcon}
+                icon={faCalendar}
+              />
+              {release.releaseYear}
+            </h4>
+          )}
+        </div>
+      ) : (
+        <div className={classes.releaseInfo}>
+          <h3 className={classes.releaseName}>{release.spotifyAlbumName}</h3>
+          {release.spotifyArtist && (
+            <h4 className={classes.releaseDetail}>
+              <FontAwesomeIcon
+                className={classes.releaseDetailIcon}
+                icon={faUser}
+              />
+              {release.spotifyArtist}
+            </h4>
+          )}
+          {release.releaseCountry && (
+            <h4 className={classes.releaseDetail}>
+              <FontAwesomeIcon
+                className={classes.releaseDetailIcon}
+                icon={faGlobe}
+              />
+              {release.releaseCountry}
+            </h4>
+          )}
+          {release.releaseYear && (
+            <h4 className={classes.releaseDetail}>
+              <FontAwesomeIcon
+                className={classes.releaseDetailIcon}
+                icon={faCalendar}
+              />
+              {release.releaseYear}
+            </h4>
+          )}
+        </div>
       )}
-      {release.releaseCountry && (
-        <h4 className={classes.releaseDetail}>
-          <FontAwesomeIcon
-            className={classes.releaseDetailIcon}
-            icon={faGlobe}
-          />
-          {release.releaseCountry}
-        </h4>
-      )}
-      {release.releaseYear && (
-        <h4 className={classes.releaseDetail}>
-          <FontAwesomeIcon
-            className={classes.releaseDetailIcon}
-            icon={faCalendar}
-          />
-          {release.releaseYear}
-        </h4>
-      )}
-    </div>
+    </>
   );
 };
 
